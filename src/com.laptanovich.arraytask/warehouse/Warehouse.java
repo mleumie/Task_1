ef217ae5db429b1ce3de3d68b10e7afd204ef796
Aliaksei <Laptanto@gmail.com>
2026-04-15 19:03:09 +0300

    Add files via upload

--- src/com.laptanovich.arraytask/warehouse/Warehouse.java
+++ src/com.laptanovich.arraytask/warehouse/Warehouse.java
@@ -1,14 +1,28 @@
-package com.laptanovich.arraytask.warehouse;
+package com.laptanovich.arraytask.warehouse.impl;
 
 import com.laptanovich.arraytask.entity.ArrayStatistics;
-import com.laptanovich.arraytask.warehouse.impl.WarehouseImpl;
 
-public interface Warehouse {
-    public static WarehouseImpl getInstance() {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Warehouse {
+    private static Warehouse instance;
+    private Map<Integer, ArrayStatistics> stats = new HashMap<>();
+
+    private Warehouse() {}
+
+    public static Warehouse getInstance() {
+        if (instance == null) {
+            instance = new Warehouse();
+        }
+        return instance;
     }
 
-    public void put(int id, ArrayStatistics arrayStatistics);
+    public void put(int id, ArrayStatistics arrayStatistics) {
+        stats.put(id, arrayStatistics);
+    }
 
-    public ArrayStatistics get(int id);
-
+    public ArrayStatistics get(int id) {
+        return stats.get(id);
+    }
 }
